3dbb16a4e67e13e4e152e471c59e32959c60c90e
Nimira43 <n.rabone72@aol.com>
2024-11-28 01:34:09 +0000

    Printing out more meaningful data - the blockchain and its blocks.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 class Block:
   def __init__(self, data):
     self.data = data
+  
+  def __repr__(self):
+    return f'Block - data: {self.data}'
 
 class Blockchain:
   def __init__(self):
@@ -8,6 +11,9 @@ class Blockchain:
 
   def add_block(self, data):
     self.chain.append(Block(data))
+
+  def __repr__(self):
+    return f'Blockchain: {self.chain}'
 
 blockchain = Blockchain()
 blockchain.add_block('one') 
