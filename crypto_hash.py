2841ddda4479fdfb416b47ab4e1b2a7a14b893a9
Nimira43 <n.rabone72@aol.com>
2025-01-27 02:33:42 +0000

    Using json.dumps() to ensure that input data is converted into a consistent string format before hashing

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,10 +1,13 @@
 import hashlib
+import json
 
 def crypto_hash(data):
-  return hashlib.sha256(data.encode('utf-8')).hexdigest()
+  stringified_data = json.dumps(data)
+
+  return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
 
 def main():
-  print(f"crypto_hash('test): {crypto_hash('test')}")
+  print(f"crypto_hash(4): {crypto_hash(4)}")
 
 if __name__ == '__main__':
   main()
